f4be9e53b5c296ee4b00069619fd485d6b6cd74f
Artem Kamysh <a.kamysh@corp.mail.ru>
2020-03-16 03:44:44 +0300

    Telegram notifications supported

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 const https = require('https');
 const { parse } = require('node-html-parser');
+const TelegramBot = require('node-telegram-bot-api');
+const Agent = require('socks5-https-client/lib/Agent');
+const processDotEnv = require('dotenv').config();
 
 // #todo если цена упала до минимума, минимум уменьшается на subtractionStepFromMin
-// #todo рассылка нотификаций через бота в телеграмм
+// #todo подключить базу ждя сохраниения айдишек пользователей
+// #todo после сохранения айдишек пользователей можно будет
+// организовать exchangeMin, realMin, priceCheckRetryTime, subtractionStepFromMin для
+// каждого свои.
+
+const token = processDotEnv.parsed.botToken;
+
+const bot = new TelegramBot(token, {
+    polling: true,
+    request: {
+        agentClass: Agent,
+		agentOptions: {
+			socksHost: processDotEnv.parsed.proxySocks5Host,
+			socksPort: processDotEnv.parsed.proxyPort,
+			socksUsername: processDotEnv.parsed.proxyUsername,
+			socksPassword: processDotEnv.parsed.proxyPassword,
+		}
+    },
+});
 
 // preferences
 const preferences = {
@@ -10,7 +31,9 @@ const preferences = {
     realMin: 280000,
     priceCheckRetryTime: 5000,
     subtractionStepFromMin: 30000,
-}
+};
+
+const users = new Set();
 
 // http reqs
 const exchangeCoinUrl = 'https://localbitcoins.net/';
@@ -21,23 +44,25 @@ const exchangeCoin = httpsRequest(exchangeCoinUrl)
 const realCoinRateUrl = 'https://blockchain.info/ru/ticker';
 const realCoinRate = httpsRequest(realCoinRateUrl)
     .then(getRealStockPrice)
-    .catch((e) => console.log('e: ', e))
+    .catch((e) => console.log('e: ', e));
 
-setInterval(
-    getAlertIfPriceHasFallen,
-    preferences.priceCheckRetryTime,
-)
-
-function getAlertIfPriceHasFallen () {
-    Promise.all([exchangeCoin, realCoinRate]).then(([exchangeRate, realRate]) => {
-        if (exchangeRate < preferences.exchangeMin) {
-            console.log('Цена в обменнике упала до: ', exchangeRate);
-        };
-        if (realRate < preferences.realMin) {
-            console.log('Курс монеты упал до: ', realRate);
-        };
+function getRealRate () {
+    return Promise.resolve(realCoinRate).then((realRate) => {
+        return realRate;
     });
-}
+};
+
+function getExchangeRate () {
+    return Promise.resolve(exchangeCoin).then((realRate) => {
+        return realRate;
+    });
+};
+
+function getRealAndExchangeRate () {
+    return Promise.all([exchangeCoin, realCoinRate]).then(([exchangeCoinRate, realCoinRate]) => {
+        return { exchangeCoinRate, realCoinRate };
+    });
+};
 
 // http reqs promise wrapper
 function httpsRequest (params, postData) {
@@ -53,7 +78,6 @@ function httpsRequestCallback ({resolve, reject}, {params, postData}) {
         }
 
         let body = [];
-
         res.on('data', (chunk) => {
             body.push(chunk);
         });
@@ -77,7 +101,32 @@ function httpsRequestCallback ({resolve, reject}, {params, postData}) {
     }
 
     req.end();
-}
+};
+
+// general logic
+setInterval(
+    getAlertIfPriceHasFallen,
+    preferences.priceCheckRetryTime,
+);
+
+async function getAlertIfPriceHasFallen () {
+    const { exchangeCoinRate, realCoinRate } = await getRealAndExchangeRate();
+
+    console.log('P', processDotEnv)
+
+    users.forEach((userId) => {
+        if (exchangeCoinRate < preferences.exchangeMin) {
+            bot.sendMessage(userId,
+                rateView('Срочно! Цена в обменнике упала до: ', exchangeCoinRate)
+            );
+        }
+        if (realCoinRate < preferences.realMin) {
+            bot.sendMessage(userId,
+                rateView('Срочно! Курс монеты упал до: ', realCoinRate)
+            );
+        }
+    })
+};
 
 // exchange logic
 function getStockPriceForBuying (htmlStr) {
@@ -123,4 +172,89 @@ function getRealStockPrice (jsonStr) {
 
 function getPriceByCountry (jsonWithPrices, targetCountry) {
     return jsonWithPrices[targetCountry].last;
+};
+
+// routing
+bot.onText(/\/start/, async (msg) => {
+    // console.log('msg', msg.from.id);
+    const startData = [
+        {
+            name: '/rate',
+            description: 'Узнать цену биткоина на бирже и в обменниках',
+        },
+        {
+            name: '/exchangerate',
+            description: 'Узнать цену биткоина в обменниках',
+        },
+        {
+            name: '/realrate',
+            description: 'Узнать цену биткоина на бирже',
+        },
+    ];
+    const chatId = msg.chat.id;
+    users.add(msg.from.id);
+
+    bot.sendMessage(chatId,
+        startView(startData)
+    );
+});
+
+bot.onText(/\/rate/, async (msg) => {
+    const { exchangeCoinRate, realCoinRate } = await getRealAndExchangeRate();
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId,
+        rateView('Курс биткоина на бирже: ', realCoinRate)
+            .concat('\n', rateView('Курс биткоина в обменниках: ', exchangeCoinRate)
+        )
+    );
+});
+
+bot.onText(/\/realrate/, async (msg) => {
+    const realCoinRate = await getRealRate();
+    const chatId = msg.chat.id;
+    
+    bot.sendMessage(chatId,
+        rateView('Курс биткоина на бирже: ', realCoinRate)
+    );
+});
+
+bot.onText(/\/exchangerate/, async (msg) => {
+    const exchangeCoinRate = await getExchangeRate();
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId,
+        rateView('Курс биткоина в обменниках: ', exchangeCoinRate)
+    );
+});
+
+// views
+function startView (data) {
+    const descriptionText = `
+Доброго дня!
+    
+Этот бот позволяет моментально узнавать о скачках
+курса биткоина на бирже и в обменниках.
+А также самостоятельно уведомляет, когда
+биткоин активно растет или падает.
+    
+Бот может:
+`
+
+    return descriptionText.concat(
+        data.map((el) => {
+            return el.name.concat(' - ', el.description);
+        }).join('\n')
+    )
+};
+
+function rateView (text, coinNumber) {
+    return text.concat(
+        beautyCoinNumberView(coinNumber),
+        '₽',
+    );
+};
+
+function beautyCoinNumberView (coinNumber) {
+    return coinNumber.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
 };
